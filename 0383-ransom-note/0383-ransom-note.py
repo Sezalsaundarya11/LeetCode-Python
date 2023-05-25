889e14eacd0427a71189867a58e01f8c64991557
Sezal Saundarya <87928300+Sezalsaundarya11@users.noreply.github.com>
2023-05-25 20:47:14 +0530

    Time: 29 ms (93.18%), Space: 13.4 MB (90.87%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type ransomNote: str
         :type magazine: str
         :rtype: bool
-        """
+       
 
         # using counter
 
@@ -13,4 +13,15 @@ class Solution(object):
         if count1 & count2 == count1:
             return True
         return False
+         """
+
+        #  using list
+        for letter in ransomNote:
+            if letter in magazine:
+                magazine = magazine.replace(letter," ",1)
+            else:
+                return False
+        return True
+
+
 
