4e58d051d34335318bb72db1ce0aa5480221ff18
Sezal Saundarya <87928300+Sezalsaundarya11@users.noreply.github.com>
2023-05-25 23:56:01 +0530

    Time: 44 ms (57.08%), Space: 15.4 MB (8.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,12 +7,16 @@ class Solution(object):
         """
         if(len(s)!=len(t)):
             return False
+        freq=[0]*26
 
-        list1 = list(s)
+        for i in range(0, len(s)):
+            freq[ord(s[i])-ord('a')]+=1
+            freq[ord(t[i])-ord('a')]-=1
+        
+        for j in range(0, len(freq)):
+           if(freq[j]<0):
+               return False
 
-        for i in s:
-            if i in t:
-                t=t.replace(i,'',1)
-            else:
-                return False
         return True
+
+        
